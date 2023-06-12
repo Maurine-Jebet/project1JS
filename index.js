e1c0d6ea61f2715ece7b64dbcadd70b63ef89982
Maurine-Jebet <maurinejebett@gmai.com>
2023-06-12 13:56:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+let face=new Typed('.beauty',{
+    strings:['Glow Is The Essence Of Beauty'],
+    typedSpeed:20,
+    backSpeed:200,
+    backDelay:50,
+    loop:true
+})
+// let bbody = new Typed('.care',{
+    
+//     strings:['A Lotion is a tropica preparation ,applied to the skin<br> with bare hands or cotton wool,with the intent to <br>moisturise and treat the skin that most body lotions <br>are meant to simply keep the skin soft'],
+//     typedSpeed:50,
+//     backSpeed:300,
+//     backDelay:200,
+//     loop:true
+
+//   })
+let body = new Typed('.body',{
+    strings:['Body Skin Care'],
+    typedSpeed:50,
+    backSpeed:300,
+    backDelay:200,
+    loop:true
+
+})  
+let skin = new Typed('.skin',{
+    strings:['Face skin care'],
+    typedSpeed:50,
+    backSpeed:300,
+    backDelay:200,
+    loop:true
+
+})  
+// let appearance = new Typed('.appearance',{
+//     strings:['Is a range of practices that support skin integrity<br>enhance its appearance and relieve skin conditions<br>They can include nutrition,avoidance of excessive sun <br>exposure and approprite use of emoilients'],
+//     typedSpeed:50,
+//     backSpeed:300,
+//     backDelay:200,
+//     loop:true
+
+// }) 
+
+
+
+
+
+
